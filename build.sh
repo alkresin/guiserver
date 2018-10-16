7d72ba12089e37563f946e75aa8abdfd3abf5e97
Alexander S.Kresin <alkresin@yahoo.com>
2018-10-16 16:29:28 +0300

    Linux build scripts fixed

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ export HWGUI_LIBS="-lhwgui -lprocmisc -lhbxml -lhwgdebug"
 
 $HB_INS/bin/linux/gcc/harbour guiserver -n  -q -i$HB_INS/include -i$HWGUI_INC -w2 $1 2>bldh.log
 
-gcc guiserver.c hbip.c listen.c -oguiserver  -I $HB_INS/include -I $HWGUI_INC -I ../../../source/gtk -DHWG_USE_POINTER_ITEM -L $HB_INS/lib/linux/gcc -L $HWGUI_LIB $SYSTEM_LIBS -Wl,--start-group $HWGUI_LIBS $HARBOUR_LIBS -Wl,--end-group `pkg-config --cflags gtk+-2.0` `pkg-config gtk+-2.0 --libs`  >bld.log 2>bld.log
+gcc guiserver.c hbip.c listen.c -oguiserver  -I $HB_INS/include -I $HWGUI_INC -I ../../../source/gtk -DHWG_USE_POINTER_ITEM -L $HB_INS/lib/linux/gcc -L $HWGUI_LIB -Wl,--start-group $HWGUI_LIBS $HARBOUR_LIBS $SYSTEM_LIBS -Wl,--end-group `pkg-config --cflags gtk+-2.0` `pkg-config gtk+-2.0 --libs`  >bld.log 2>bld.log
 
 rm guiserver.c
 rm *.o
--- tests/bldtest.sh
+++ tests/bldtest.sh
@@ -5,7 +5,7 @@ export HARBOUR_LIBS="-lhbdebug -lhbvm -lhbrtl -lgttrm -lhblang -lhbrdd -lhbmacro
 
 $HB_INS/bin/linux/gcc/harbour $1.prg ../extgui.prg ../extgui_classes.prg -n  -q -i$HB_INS/include -w2 $1 2>bldh.log
 
-gcc $1.c extgui.c extgui_classes.c ../hbip.c ../listen.c ../misc.c -o$1  -I $HB_INS/include -DHWG_USE_POINTER_ITEM -L $HB_INS/lib/linux/gcc $SYSTEM_LIBS -Wl,--start-group $HARBOUR_LIBS -Wl,--end-group  >bld.log 2>bld.log
+gcc $1.c extgui.c extgui_classes.c ../hbip.c ../listen.c ../misc.c -o$1  -I $HB_INS/include -DHWG_USE_POINTER_ITEM -L $HB_INS/lib/linux/gcc -Wl,--start-group $HARBOUR_LIBS $SYSTEM_LIBS -Wl,--end-group  >bld.log 2>bld.log
 
 rm $1.c
 rm extgui.c
